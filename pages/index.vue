bc9541cfd5f22c66d3c61328a2e399744f21e4a4
Sun-Tang <3826287@qq.com>
2022-12-13 23:48:53 +0900

    安装提高开发效率插件配置

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>hello</div>
+  <div @click="handleClick">首页</div>
+  <h1>{{ xd }}</h1>
+  <a-button type="primary">按钮</a-button>
 </template>
 
-<script setup></script>
+<script setup>
+const xd = $ref(1)
+
+const handleClick = () => {
+  xd++
+}
+</script>
 
 <style lang="scss" scoped></style>
