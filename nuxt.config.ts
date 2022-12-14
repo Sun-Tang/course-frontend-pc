92cc641e69616d8df68216eb3cde5e865fd467fc
Sun-Tang <3826287@qq.com>
2022-12-14 10:54:36 +0900

    配置pinia unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineNuxtConfig({
+  modules: ['@unocss/nuxt', '@pinia/nuxt'],
   experimental: {
     reactivityTransform: true
   },
@@ -10,6 +11,45 @@ export default defineNuxtConfig({
       Components({
         resolvers: [AntDesignVueResolver()]
       })
+    ]
+  },
+  unocss: {
+    uno: true, // 激活unocss
+    attributify: true, // 激活属性化模式
+    shortcuts: [
+      // 自定义快捷方式
+      { flexc: 'flex items-center justify-center' },
+      { flexb: 'flex items-center justify-between' },
+      { btn: 'rounded-5px text-center cursor-pointer select-none' },
+      { 'text-btn': 'text-center cursor-pointer select-none' }
+    ],
+    rules: [
+      //自定义规则
+      [
+        /^truncate-(\d+)$/,
+        ([, d]) => ({
+          overflow: 'hidden',
+          display: '-webkit-box',
+          'text-overflow': 'ellipsis',
+          '-webkit-line-clamp': d,
+          '-webkit-box-orient': 'vertical'
+        })
+      ],
+      [
+        /^center-text-(\d+)$/,
+        ([, d]) => ({
+          height: `${d}px`,
+          'align-items': 'center',
+          'line-height': `${Number(d) - 2}px`
+        })
+      ],
+      [
+        /^wh-(\d+)$/,
+        ([, d]) => ({
+          width: `${d}px`,
+          height: `${d}px`
+        })
+      ]
     ]
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div @click="handleClick">首页</div>
-  <h1>{{ xd }}</h1>
-  <a-button type="primary">按钮</a-button>
+  <div class="home">
+    <div wh-200 bg-blue font-600 color-red flexc>小滴课堂</div>
+    <a-button type="primary" bg-green! color-red>按钮</a-button><br />
+    <h1>当前pinia的初始化值{{ count }}</h1>
+    <h2 @click="addClick">增加</h2>
+    <h2>当前pinia的计算的值：{{ multiplyCount }}</h2>
+  </div>
 </template>
 
-<script setup>
-const xd = $ref(1)
-
-const handleClick = () => {
-  xd++
-}
+<script lang="ts" setup>
+const { count, add, multiplyCount } = $(useUser())
+const addClick = () => add()
 </script>
 
 <style lang="scss" scoped></style>
